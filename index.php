ba679e9425ed85df4cb03688c31a47a05360cda6
Conobi <Conobi@users.noreply.github.com>
2017-05-20 18:36:18 +0200

    Fixed not configured exception handler

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
 include 'includes/Helpers.php';
 $test_url = (isset($_SERVER['HTTPS']) ? "https" : "http") . "://".$_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI'];
-if($test_url != $config['base_url'].'/' xor $test_url != $config['base_url'].'/index.php'){} else {
+if($test_url != $config['base_url'] xor $test_url != $config['base_url'].'/index.php'){} else {
   $config['base_url'] = str_replace('/index.php', '/', $test_url);
   trigger_error($lang['url_not_configured']);
 }
